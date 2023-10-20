c3cb017b8969fcf4363483116c938a7b7603b01b
Vishal Kumar <vishalraj232@gmail.com>
2023-10-20 05:04:40 +0000

    operations half done

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -32,3 +32,28 @@ let someRandomNumber = 44
 let stringNumber = String(someRandomNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+// ****************************************** Operations *****************************************
+
+let value = 3
+
+let negValue = -value
+
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "vishal"
+let str2 = "kumar"
+let str3 = str1+str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
