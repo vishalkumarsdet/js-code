d7551df93bbcc19d5b421a68b1d1692798b4e4c1
Vishal Kumar <vishalraj232@gmail.com>
2023-10-20 04:22:41 +0000

    dataType done

---
+++ basics/dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version
+
+// alert( 3 + 3)  // we are using Nodejs, Not browser, Hence it will throw error.
+
+let name = "Vishal"
+let age = 24
+let isLoggedIn = false
+/* 
+primitive dataType
+// number => 2 to power 53 (it is a range of number)
+// bigint
+// string => ""
+// boolean => true|false
+// null => standalone value
+// undefined => value not assigned till now to this variable
+// symbol => unique
+*/
+
+// object
+
+console.log(typeof age);
+console.log(typeof null); // Object
+console.log(typeof undefined); // undefined
