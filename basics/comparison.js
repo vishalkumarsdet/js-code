912416810138fc179c03b6665d0fd299f91fbf9e
Vishal Kumar <vishalraj232@gmail.com>
2023-10-20 16:47:15 +0000

    coparison done

--- basics/comparison.js
+++ basics/comparison.js
@@ -7,3 +7,11 @@ console.log(null >= 0);
 // The reason is that an equality check == and comparison > < >= <= work differently.
 // Comparison covert null to a number, treating it as 0.
 // That's why (3) null >=0 is true and (1) null >0 is false.
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===
+
+console.log("2" === 2); // Strick check. comparison value as well as dataType
