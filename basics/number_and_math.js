0687d950b97e7b83c9b131830f26f446298e7be1
Vishal Kumar <vishalraj232@gmail.com>
2023-10-21 06:22:07 +0000

    added

--- basics/number_and_math.js
+++ basics/number_and_math.js
@@ -23,7 +23,7 @@ console.log(Math);
 console.log(Math.abs(-4));
 console.log(Math.round(4.3));
 console.log(Math.ceil(4.2));
-console.log(Math.floor(4.9));
+console.log(Math.floor(4.9)); 
 console.log(Math.min(4, 3, 6, 8));
 console.log(Math.max(4, 3, 6, 8));
 console.log(Math.random());
